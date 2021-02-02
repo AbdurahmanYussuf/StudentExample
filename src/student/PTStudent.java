a2ef0e8693dd74566abdf96e6907822705bb6762
A Yussuf <abdi_arshavin@live.ca>
2021-02-02 11:14:27 -0500

    Commit #4 PT Student

---
+++ src/student/PTStudent.java
@@ -0,0 +1,16 @@
+/*
+ Adding code on PTStudent branch - Commit #4
+ */
+package student;
+
+/**
+ *
+ * @author abdi_
+ */
+public class PTStudent extends Student {
+    
+    public PTStudent(String name, int id) {
+        super(name, id);
+    }
+    
+}
